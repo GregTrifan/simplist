3d3499f0c610475eeef9035c2e60ce10ca4d027c
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-06-07 17:05:39 +0300

    🎉 Repo init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <View msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import './index.css'
+import View from './components/View.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    View
+  }
+})
+</script>
+
---
+++ src/components/View.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1 class="text-5xl font-bold">{{ msg }}</h1>
+  <p>Lorem Ipsum dolor sit amet</p>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'View',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
